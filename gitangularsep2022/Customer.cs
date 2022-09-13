b9ceb3cfef76dc22c9f0e134a2f09d819e3040bb
eswaribala <parameswaribala@gmail.com>
2022-09-14 00:02:13 +0530

    file updated by user1

---
+++ gitangularsep2022/Customer.cs
@@ -0,0 +1,15 @@
+Class Customer{
+
+public String CustomerId{get;set;}
+
+# developer 1
+#developer 2
+public String CustomerName{get;set;}
+
+
+public String Email{get;set;}
+public String DOB {get;set;}
+public String Gender{get;set;}
+#user1
+public long Salary{get;set;}
+}
